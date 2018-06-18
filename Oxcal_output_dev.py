4ec696a020964cb74027ab51b24e9137d54dbc2b
Matthew Fort <fort1@illinois.edu>
2018-06-18 08:46:03 -0500

    New commit adds ability to pull posterior information from Oxcal .js / .json files. Created functions to handle cleaning and writing to excelsheet of median dates and probability ranges.

--- Oxcal_output_dev.py
+++ Oxcal_output_dev.py
@@ -57,8 +57,10 @@ else:
           
 #Create Woorkbooks for Oxcal_Data using xlsxwriter
 workbook = xlsxwriter.Workbook(excel_filename)
-worksheet = workbook.add_worksheet()
+Date_ranges = workbook.add_worksheet()
 worksheet2 = workbook.add_worksheet()
+PDF_Graphing = workbook.add_worksheet()
+
 
 #Name varibles for various cell formats
 header = workbook.add_format({'bold': True, 'center_across': True})
@@ -76,49 +78,41 @@ sheet2_row1 = 0
 sheet2_col1 = 0
 sheet2_row_adj = 0
 row_count = []
+number_samples = 0
 
-Labels = ['Name', 'RYCBP', '±', '1s.d. Cal', '%', '2s.d. Cal', '%', 'Median', '±']
+Labels = ['Name', 'RYCBP', 'Plus or Minus', '1s.d. Cal', '%', '2s.d. Cal', '%', 'Median', 'Plus or Minus', '','Posterior', '1s.d. Cal', '%', '2s.d. Cal', '%', 'Median', 'Plus or Minus', 'Agreement', 'Convergence', 'probNorm' ]
 for count, name in enumerate(Labels):
-    worksheet.write(sheet1_row1, sheet1_col1+count, name, header)
+    Date_ranges.write(sheet1_row1, sheet1_col1+count, name, header)
     
-worksheet.set_column('D:D', 13)
-worksheet.set_column('F:F', 13)
+Date_ranges.set_column('D:D', 13)
+Date_ranges.set_column('F:F', 13)
 
+#Advance and create row varibles necessary for age ranges and percentages; create global c varible
 sheet1_row1+=1
 sheet1_row2+=1
+sheet1_row3 = 1
+sheet1_row4 = 1
 
-
-#Seperate indices dictonary types from Oxcal_Data list
-for dict in Oxcal_Data[1:]:
-    IndvData = dict
-
-    list_name = IndvData['name']
-    list_date = IndvData['date']
-    list_error = IndvData['error']
-    list_liklihood = IndvData['likelihood']
-    list_range = list_liklihood['range']
-    list_median = list_liklihood['median']
-    list_sigma = list_liklihood['sigma']
-    list_prob = list_liklihood['prob']
-    list_start = list_liklihood['start']
-    list_res = list_liklihood['resolution']
+#Apply AD or BC labels to median dates
+def Medians(x, y, a, b):
+    if x > 0: 
+        AD_Median = ("AD " + str(int(x)))
+        Date_ranges.write(sheet1_row1, sheet1_col1+a, AD_Median, center)
+        Date_ranges.write(sheet1_row1, sheet1_col1+b, y, format)
+    elif x < 0:
+        BC_Median = ("BC " + str(int(abs(x))))
+        Date_ranges.write(sheet1_row1, sheet1_col1+a, BC_Median, center)
+        Date_ranges.write(sheet1_row1, sheet1_col1+b, y, format)
+        
+    return
     
-    worksheet.write(sheet1_row1, sheet1_col1, list_name)
-    worksheet.write(sheet1_row1, sheet1_col1+1, list_date, format)
-    worksheet.write(sheet1_row1, sheet1_col1+2, list_error, format)
+#Seperate and write to spreadsheet probabilities
+def Ranges(range, indpos1, indpos2, col1, col2, row):
     
-    #Apply AD or BC labels to median dates
-    if list_median > 0: 
-        AD_Median = ("AD " + str(int(list_median)))
-        worksheet.write(sheet1_row1, sheet1_col1+7, AD_Median, center)
-        worksheet.write(sheet1_row1, sheet1_col1+8, list_sigma, format)
-    elif list_median < 0:
-        BC_Median = ("BC " + str(int(abs(list_median))))
-        worksheet.write(sheet1_row1, sheet1_col1+7, BC_Median, center)
-        worksheet.write(sheet1_row1, sheet1_col1+8, list_sigma, format)
+    global c
+    c = row
     
-    #Seperate and write to spreadsheet 1 sigma probs
-    for prob in list_range[1:2]:
+    for prob in range[indpos1:indpos2]:
         step_in_1a = prob
         #print (step_in_1a)
         for sets in step_in_1a:
@@ -128,107 +122,149 @@ for dict in Oxcal_Data[1:]:
             step_in_3c = (step_in_2a[2]/100)
             
             if step_in_3a >= 0:
-                AD_Date = ('AD ' + str(int(step_in_3a)) + '-' + str(int(step_in_3b)))
-                worksheet.write(sheet1_row1, sheet1_col1+3, AD_Date, center)
+                AD_Date = ('A.D. ' + str(int(step_in_3a)) + '- A.D. ' +    
+                str(int(step_in_3b)))
+                Date_ranges.write(c, sheet1_col1+col1, AD_Date, center)
                 
             elif step_in_3a <= 0 and step_in_3b <= 0:
                 step_in_3a = abs(step_in_3a)
                 step_in_3b = abs(step_in_3b)
                 
-                BC_Date = ('BC ' + str(int(step_in_3a)) + '-' + str(int(step_in_3b)))
-                worksheet.write(sheet1_row1, sheet1_col1+3, BC_Date, center)
+                BC_Date = ('B.C. ' + str(int(step_in_3a)) + '- B.C. ' + 
+                str(int(step_in_3b)))
+                Date_ranges.write(c, sheet1_col1+col1, BC_Date, center)
                 
             else:
                 step_in_3a = abs(step_in_3a)
                 
-                BC_AD_Date = ('BC ' + str(int(step_ind_3a)) + '-AD '+ str(int(step_in_3b)))
-                worksheet.write(sheet1_row1, sheet1_col1+3, BC_AD_Date, center)
+                BC_AD_Date = ('B.C. ' + str(int(step_ind_3a)) + '- A.D. '+ 
+                str(int(step_in_3b)))
+                Date_ranges.write(c, sheet1_col1+col1, BC_AD_Date, center)
             
-            worksheet.write(sheet1_row1, sheet1_col1+4, step_in_3c, percent)
-           
-            sheet1_row1 += 1
+            Date_ranges.write(c, sheet1_col1+col2, step_in_3c, percent)
             
-    #Seperate and write to spreadsheet 2 sigma probs        
-    for prob in list_range[2:3]:
-        step_in_1b = prob
-        #print (step_in_1b)
-        for sets in step_in_1b:
-            step_in_2b = sets
             
-            step_in_3d = step_in_2b[0]
-            step_in_3e = step_in_2b[1]
-            step_in_3f = (step_in_2b[2]/100)
+            c += 1
             
-            if step_in_3d >0:
-                AD_Date = ('AD ' + str(int(step_in_3d)) + '-' + str(int(step_in_3e)))
-                worksheet.write(sheet1_row2, sheet1_col1+5, AD_Date, center)
-                
-            elif step_in_3d <= 0 and step_in_3e <= 0:
-                step_in_3d = abs(step_in_3d)
-                step_in_3e = abs(step_in_3e)
-                
-                BC_Date = ('BC ' + str(int(step_in_3d)) + '-' + str(int(step_in_3e)))
-                worksheet.write(sheet1_row2, sheet1_col1+5, BC_Date, center)
-                
-            else:
-                step_in_3d = abs(step_in_3d)
-                
-                BC_AD_Date = ('BC ' + str(int(step_in_3d)) + '-AD '+ str(int(step_in_3e)))
-                
-                worksheet.write(sheet1_row2, sheet1_col1+5, BC_AD_Date, center)
-            
-           
-            worksheet.write(sheet1_row2, sheet1_col1+6, step_in_3f, percent)
-            
-            sheet1_row2 += 1
+        return
+
+#Determine 
+def Row_Shift (x, y):
+    
+    global rowshift1
+    global rowshift2
+    
+    rowshift1 = x
+    rowshift2 = y
+    
+    Row_Adj = rowshift1 - rowshift2
+    
+    if Row_Adj < 0:
+        rowshift1 = rowshift1 + abs(Row_Adj) + 1
+        rowshift2 += 1
+    if Row_Adj > 0:
+        rowshift2 = rowshift2 + abs(Row_Adj) + 1
+        rowshift1 += 1
+    if Row_Adj == 0:
+        rowshift1 += 1
+        rowshift2 += 1
         
-    #Adjust sheet1_row1 or sheet1_row2 to keep a consistent 1 row space between samples
-    sheet1_row_adj = sheet1_row1 - sheet1_row2
-    if sheet1_row_adj < 0:
-        sheet1_row1 = sheet1_row1 + abs(sheet1_row_adj)+1
-        sheet1_row2 += 1
-    elif sheet1_row_adj > 0:
-        sheet1_row2 = sheet1_row2 + abs(sheet1_row_adj)+1
-        sheet1_row1 += 1
-    elif sheet1_row_adj == 0:
-        sheet1_row1 += 1
-        sheet1_row2 += 1
+    return
+
+#Seperate indices dictonary types from Oxcal_Data list
+for dict in Oxcal_Data[0:]:
+    IndvData = dict
     
-    #Set headers for worksheet2 and advance row1
-    header_dates = (list_name + ' dates')
-    header_prob = (list_name + ' prob')
+    list_liklihood = IndvData['likelihood']
+    list_comment = list_liklihood['comment']
     
-    worksheet2.write(sheet2_row1, sheet2_col1, header_dates, header)
-    sheet2_col1 += 1
-    worksheet2.write(sheet2_row1, sheet2_col1, header_prob, header)
-    sheet2_row1 += 1
+    #very janky way to get around varibles that don't exist in the first few items in the list
+    if list_comment[0] == "OxCal v4.3.2 Bronk Ramsey (2017); r:5":
+        continue
     
-    #write probability density to worksheet2 and count number of rows
-    for prob in list_prob:
-        worksheet2.write(sheet2_row1, sheet2_col1, prob)
-        sheet2_row1 += 1
-        sheet2_row_adj += 1
-        row_count.append(sheet2_row_adj)
-        
-    #reset worksheet2 row1 back to 1 and create seperate variable for list of start dates
-    sheet2_row1 = 1
-    start_date = list_start
+    list_op = IndvData['op']
     
-    #write dates of probability density to worksheet2 and increment up the start dates by resolution (5 years)
-    for count in row_count:
-        worksheet2.write(sheet2_row1, sheet2_col1-1, start_date)
-        start_date += list_res
-        sheet2_row1 += 1
+    if list_op == "Sequence" or list_op == "Boundary":
+        continue
+
+    #Sample Name, RCYBP date, and error on RCYBP measurment
+    list_name = IndvData['name']
+    list_date = IndvData['date']
+    list_error = IndvData['error']
     
-    #reset worksheet2 columns, rows, and row_count to necessary values for restart of loop     
-    sheet2_col1 += 1
-    sheet2_row1 = 0
-    sheet2_row_adj = 0
-    row_count = []
+    #Unmodeled Range, Median, Sigma, Probability, Range Start, and Resolution
+    unmodeled_range = list_liklihood['range']
+    unmodeled_median = list_liklihood['median']
+    unmodeled_sigma = list_liklihood['sigma']
+    list_prob = list_liklihood['prob']
+    list_start = list_liklihood['start']
+    list_res = list_liklihood['resolution']
+    
+    #Modeled Range, Median, Sigma, Probability, 
+    list_posterior = IndvData['posterior']
+    modeled_range = list_posterior['range']
+    modeled_median = list_posterior['median']
+    modeled_sigma = list_posterior['sigma']
+    modeled_agreement = list_posterior['agreement']
+    modeled_probNorm = list_posterior['probNorm']
+    modeled_convergence = list_posterior['convergence']
+    
+    Date_ranges.write(sheet1_row1, sheet1_col1, list_name)
+    Date_ranges.write(sheet1_row1, sheet1_col1+1, list_date, format)
+    Date_ranges.write(sheet1_row1, sheet1_col1+2, list_error, format) 
+    Date_ranges.write(sheet1_row1, sheet1_col1+17, modeled_agreement, format) #need another digit
+    Date_ranges.write(sheet1_row1, sheet1_col1+18, modeled_convergence, format) #need another digit
+    Date_ranges.write(sheet1_row1, sheet1_col1+19, modeled_probNorm, format) #need 7 digits
+    
+    #Writing Unmodeled and Modeled Medians and Sigmas
+    Medians(unmodeled_median, unmodeled_sigma, 7, 8)
+    Medians(modeled_median, modeled_sigma, 15, 16)
+    
+    #Writing Unmodeled for 1 and 2 sigma
+    Ranges(unmodeled_range, 1, 2, 3, 4, sheet1_row1)
+    sheet1_row1 = c
+    Ranges(unmodeled_range, 2, 3, 5, 6, sheet1_row2)
+    sheet1_row2 = c
+    Ranges(modeled_range, 1, 2, 11, 12, sheet1_row3)
+    sheet1_row3 = c
+    Ranges(modeled_range, 2, 3, 13, 14, sheet1_row4)
+    sheet1_row4 = c
+    
+    #Adjust sheet_row_num values to keep a consistent 1 row space between samples
+    
+    Row_Shift(sheet1_row1, sheet1_row2)
+    sheet1_row1 = rowshift1
+    sheet1_row2 = rowshift2
+    
+    Row_Shift(sheet1_row3, sheet1_row4)
+    sheet1_row3 = rowshift1
+    sheet1_row4 = rowshift2
+    
+    Row_adj = sheet1_row2 - sheet1_row3
+    if Row_adj < 0:
+        sheet1_row2 = sheet1_row2 + abs(Row_adj)
+        sheet1_row1 = sheet1_row2
+    if Row_adj > 0: 
+        sheet1_row3 = sheet1_row3 + abs(Row_adj)
+        sheet1_row4 = sheet1_row3
+    if Row_adj == 0:
+        sheet1_row2 = sheet1_row3
 
 #Add reference to calibration software used
-worksheet.write(sheet1_row1, sheet1_col1,((Oxcal_Data[0]['likelihood']['comment'][0]) + (Oxcal_Data[0]['likelihood']['comment'][1])), italics) 
+#print(sheet1_row1)
+
+Date_ranges.write(sheet1_row1, sheet1_col1,((Oxcal_Data[0]['likelihood']['comment'][0]) + (Oxcal_Data[0]['likelihood']['comment'][1])), italics) 
                     
 workbook.close()
 
 
+        
+'''     This is something to play around with, perhaps if I make this all a function I could include agruments about which prob density scheme to use.  
+        #write probability density to worksheet2 and count number of rows
+        for prob in list_prob:
+            mod_prob = (prob/2) + number_samples
+            worksheet2.write(sheet2_row1, sheet2_col1, mod_prob)
+            sheet2_row1 += 1
+            sheet2_row_adj += 1
+            row_count.append(sheet2_row_adj)
+'''
